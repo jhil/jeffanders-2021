cc6aa529a24f1aa91221dde750e2f38d93938056
Jeff Anders <jahilnbrand@gmail.com>
2022-02-03 12:10:40 -0700

    update font

---
+++ _js/scripts.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("button-more");
+button.addEventListener("click", function() {
+  const initiallyHidden = document.querySelectorAll('.initially-hidden');
+  for (i = 0; i < initiallyHidden.length; ++i) {
+    initiallyHidden[i].classList.add("visible");
+  }
+});
