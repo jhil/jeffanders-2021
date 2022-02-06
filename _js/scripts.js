8f6eaa67ecfeb2441b154795ef1c985f2e436807
Jeff Anders <jahilnbrand@gmail.com>
2022-02-05 22:11:09 -0700

    fix js script

--- _js/scripts.js
+++ _js/scripts.js
@@ -1,7 +1,9 @@
-const button = document.getElementById("button-more");
-button.addEventListener("click", function() {
-  const initiallyHidden = document.querySelectorAll('.initially-hidden');
-  for (i = 0; i < initiallyHidden.length; ++i) {
-    initiallyHidden[i].classList.add("visible");
-  }
+document.addEventListener("DOMContentLoaded", function () {
+	const button = document.getElementById("button-more");
+	button.addEventListener("click", function () {
+		const initiallyHidden = document.querySelectorAll(".initially-hidden");
+		for (i = 0; i < initiallyHidden.length; ++i) {
+			initiallyHidden[i].classList.toggle("hidden");
+		}
+	});
 });
